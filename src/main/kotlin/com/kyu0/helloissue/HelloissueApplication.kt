ff2c95d0c1e86ef7c28779d38425aa46e80e52f2
Kyu0 <efftar@naver.com>
2024-03-03 21:55:12 +0900

    initial commit

---
+++ src/main/kotlin/com/kyu0/helloissue/HelloissueApplication.kt
@@ -0,0 +1,11 @@
+package com.kyu0.helloissue
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HelloissueApplication
+
+fun main(args: Array<String>) {
+	runApplication<HelloissueApplication>(*args)
+}
